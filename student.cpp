6df02c5d9c399b5b8e650d3e44bc83e386c37d0b
ANYhackerfort <zhanglinghe610@gmail.com>
2024-10-15 00:18:08 -0700

    hw2

---
+++ student.cpp
@@ -0,0 +1,117 @@
+#include "student.h"
+#include <string>
+#include <cstring>
+
+int countDigit(int number) {
+  if (number == 0) return 0; 
+  return 1 + countDigit(number / 10); //Edge Case: negative ID number. 
+}
+
+char* getChar(int number) {
+  int digits = countDigit(number); 
+  char* returnChar = new char[digits + 1];
+  for (int i = 0; i < digits; i++) {
+    returnChar[i] = number % 10 + '0';
+    number = number / 10; 
+  }
+
+  for (int i = 0; i < digits / 2; i++) {
+    char temp = returnChar[i]; 
+    returnChar[i] = returnChar[digits - i - 1]; 
+    returnChar[digits - i - 1] = temp;  
+  }
+
+  returnChar[digits] = '\0';
+
+  return returnChar; 
+}
+
+Student::Student(const char * const name, int perm) {
+  this->setName(name);
+  this->setPerm(perm);
+}
+
+int Student::getPerm() const {
+  return this->perm;
+}
+
+const char * const Student::getName() const {
+  return this->name;
+}
+
+void Student::setPerm(const int permNumber) {
+  this->perm = permNumber; 
+}
+
+void Student::setName(const char * const name) {
+  this->name = new char[strlen(name)+1];
+  strcpy(this->name, name);
+}
+
+
+Student::Student(const Student &orig) {
+  this->setName(orig.getName());
+  this->setPerm(orig.getPerm());
+}
+
+Student::~Student() {
+  delete[] this->name; 
+}
+
+Student & Student::operator=(const Student &right) {
+  // The next two lines are standard, and you should keep them.
+  // They avoid problems with self-assignment where you might free up 
+  // memory before you copy from it.  (e.g. x = x)
+
+  if (&right == this) 
+    return (*this);
+
+  // TODO... Here is where there is code missing that you need to 
+  // fill in...
+  const char* name = right.getName(); 
+  int perm = right.getPerm(); 
+
+  delete [] this->name; 
+  this->setName(name);
+  this->setPerm(perm);
+
+
+  // KEEP THE CODE BELOW THIS LINE
+  // Overloaded = should end with this line, despite what the textbook says.
+  return (*this); 
+
+}
+
+std::string Student::toString() const {
+  // int lengthOfName = strlen(this->name);
+  // int lengthOfID = countDigit(this->perm);
+
+  // int sizeReturnString = 3 + lengthOfName + lengthOfID + 1; 
+  // char returnString[sizeReturnString]; 
+
+  // returnString[0] = '['; 
+  // for (int i = 0; i < lengthOfName; i++) {
+  //   returnString[1+i] = this->name[i]; 
+  // }
+
+  // returnString[lengthOfName+1] = ','; 
+
+  // char* convertedToChar = getChar(this->perm); 
+  // for (int i = 0; i < lengthOfID; i++) {
+  //   returnString[lengthOfName + 2 + i] = convertedToChar[i]; 
+  // }
+
+  // delete[] convertedToChar;
+
+  // returnString[sizeReturnString - 2] = ']'; 
+  
+  std::string returnString = "[";
+
+  returnString += this->name;
+  returnString += ",";
+  returnString += std::to_string(this->perm);
+  returnString += "]";
+  
+  return returnString;
+}
+
---
+++ studentRoll.cpp
@@ -0,0 +1,107 @@
+#include <string>
+#include "studentRoll.h"
+
+StudentRoll::StudentRoll() {
+  head = tail = NULL;
+}
+
+void StudentRoll::insertAtTail(const Student &s) {
+  //first thing, create a deep copy of Student
+  Student* sCopy = new Student(s); 
+
+  //create the object
+  Node* newStudent = new Node(); 
+  newStudent->next = nullptr;
+  newStudent->s = sCopy;
+
+  //check if its originally empty
+  if (head == NULL) {
+    head = newStudent;
+    tail = newStudent;
+  } else {
+    tail->next = newStudent;
+    tail = tail->next; 
+  }
+}
+
+std::string StudentRoll::toString() const {
+  if (this->head == nullptr) {
+    return "[]";
+  }
+
+  std::string returnString = "["; 
+  Node* shallowHead = this->head;
+  while (shallowHead->next != nullptr) {
+    returnString += shallowHead->s->toString(); 
+    returnString += ","; 
+    shallowHead = shallowHead->next; 
+  }
+  returnString += shallowHead->s->toString(); 
+  returnString += "]";
+  return returnString;
+}
+
+StudentRoll::StudentRoll(const StudentRoll &orig) {
+  head = tail = NULL;
+  if (orig.head == nullptr) {
+    return;
+  }
+
+  Node* current = orig.head;
+  while (current != nullptr) {
+    this->insertAtTail(*current->s);
+    current = current->next; 
+  }
+
+}
+
+StudentRoll::~StudentRoll() {
+  Node* shallowHead = this->head;
+  while (shallowHead != nullptr) {
+    Node* dummyHead = shallowHead; 
+    shallowHead = shallowHead->next; 
+    delete dummyHead->s;
+    delete dummyHead;
+  }
+}
+
+StudentRoll & StudentRoll::operator =(const StudentRoll &right ) {
+  // The next two lines are standard, and you should keep them.
+  // They avoid problems with self-assignment where you might free up 
+  // memory before you copy from it.  (e.g. x = x)
+
+  if (&right == this) 
+    return (*this);
+
+  // TODO... Here is where there is code missing that you need to 
+  // fill in...
+
+  Node* shallowHead = this->head;
+  while (shallowHead != nullptr) {
+    Node* dummyHead = shallowHead; 
+    shallowHead = shallowHead->next; 
+    delete dummyHead->s;
+    delete dummyHead;  
+  }
+
+  head = tail = NULL;
+  if (right.head == nullptr) {
+    return (*this);
+  }
+
+  Node* current = right.head;
+  while (current != nullptr) {
+    this->insertAtTail(*current->s);
+    current = current->next; 
+  }
+
+  // KEEP THE CODE BELOW THIS LINE
+  // Overloaded = should end with this line, despite what the textbook says.
+  return (*this); 
+  
+}
+
+
+
+
+
